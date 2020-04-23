090c2b58f7f34d92dd4abaed90d8b61366afe970
ashmit <63419619+AshmitD@users.noreply.github.com>
2020-04-23 14:36:41 -0400

    Update form.php

--- form.php
+++ form.php
@@ -18,5 +18,5 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
-header("Location: contact.html"); 
+header('Location: contact.html'); 
 ?>
