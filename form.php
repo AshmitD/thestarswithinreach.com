37aea352ad5b8128c6353dc33299c75df61b6021
ashmit <63419619+AshmitD@users.noreply.github.com>
2020-04-23 14:39:07 -0400

    Update form.php

--- form.php
+++ form.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 $email_from = 'ashmitd1@gmail.com';
 
 
-@email_subject = 'New Form Submission';
+@email_subject = "New Form Submission";
 
 $email_body = "User name: $name./n".
     "User email: $email/n". "User message: $message./n";
@@ -18,5 +18,5 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
-header('Location: contact.html'); 
+header("Location: contact.html"); 
 ?>
