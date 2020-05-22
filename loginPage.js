2651c968bc89d6e4e199a2866cb11c61782a5bb1
ashmit <63419619+AshmitD@users.noreply.github.com>
2020-05-22 11:02:11 -0400

    Add files via upload

---
+++ loginPage.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+import {Text, View, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+
+export default function loginPage() {
+    return <View><Text>Login</Text></View>
+}
+
