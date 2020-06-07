6693232e7ccf8fedceab478972ba237366183813
Aarush Sharma <46905149+AarushSharma04@users.noreply.github.com>
2020-06-06 23:05:23 -0400

    app

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const navbar = document.querySelector(".main-nav");
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("tog");
 });
+
+window.addEventListener('scroll', function(e) {
+  const lastPosition = window.scrollY
+  if(lastPosition > 50) {
+      navbar.classList.add('back')
+  } else if (navbar.classList.contains('scrolled')) {
+      navbar.classList.remove('back')  
+  } else {
+      navbar.classList.remove('back')          
+  }
+})
